693b56cb03a9d23248615cedd791ef5434cb3bb3
abyss <matthewjyx@outlook.com>
2018-09-08 19:46:34 +0800

    欧几里得算法

---
+++ src/com/abyss/basics/EuclidAlgo.java
@@ -0,0 +1,18 @@
+package com.abyss.basics;
+
+/**
+ * @author abyss
+ * @date 2018/9/8
+ **/
+public class EuclidAlgo {
+    public static void main(String[] args) {
+        System.out.println(getGcd(9, 6));
+    }
+
+    private static int getGcd(int p, int q){
+        if (q == 0)
+            return p;
+        int r = p % q;
+        return getGcd(q, r);
+    }
+}
