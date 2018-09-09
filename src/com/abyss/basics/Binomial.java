42082c195cd5ead3486ddf18656b5eaa1c771afe
abyss <matthewjyx@outlook.com>
2018-09-09 18:41:49 +0800

    添加第一章第一节练习及二项分布递归优化算法

---
+++ src/com/abyss/basics/Binomial.java
@@ -0,0 +1,41 @@
+package com.abyss.basics;
+
+/**
+ * @author matth
+ * @date 2018/9/9
+ */
+public class Binomial {
+    private static double[][] M;
+
+    private static double binomial(int n, int k, double p) {
+        if (n == 0 && k == 0) {
+            return 1.0;
+        }
+        if (n < 0 || k < 0) {
+            return 0.0;
+        }
+        if (M[n][k] == -1) {
+            // 对于没有计算过值的才计算，计算过的直接从数组中获取
+            M[n][k] = (1.0 - p) * binomial(n - 1, k, p) + p * binomial(n - 1, k - 1, p);
+        }
+        return M[n][k];
+    }
+
+    private static double getBinomial(int n, int k, double p) {
+        M = new double[n + 1][k + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                M[i][j] = -1;
+            }
+        }
+        return binomial(n, k, p);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getBinomial(10, 5, 0.25));
+        System.out.println(getBinomial(20, 10, 0.25));
+        System.out.println(getBinomial(30, 15, 0.25));
+        System.out.println(getBinomial(50, 25, 0.25));
+        System.out.println(getBinomial(100, 50, 0.25));
+    }
+}
--- src/com/abyss/basics/FirstExercise.java
+++ src/com/abyss/basics/FirstExercise.java
@@ -1,5 +1,7 @@
 package com.abyss.basics;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -26,14 +28,14 @@ public class FirstExercise {
         return compare01(x) && compare01(y);
     }
 
-    private static void fibonacci(int n){
-        int f = 0;
-        int g = 1;
+    private static long fibonacci(int n){
+        long f = 0;
+        long g = 1;
         for (int i = 0; i <= n; i++) {
-            System.out.println(f);
             f = f + g;
             g = f - g;
         }
+        return g;
     }
 
     private static void calculateA(){
@@ -60,7 +62,7 @@ public class FirstExercise {
         return s.toString();
     }
 
-    private static void printArr(boolean[][] b){
+    private static void printArr(Boolean[][] b){
         System.out.print("\t");
         for (int i = 1; i <= b[0].length; i++) {
             System.out.print(i + "\t");
@@ -77,6 +79,47 @@ public class FirstExercise {
             System.out.println();
         }
 
+    }
+
+    private static void changeArr(){
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++) {
+            a[i] = 9 - i;
+        }
+        for (int i = 0; i < 10; i++) {
+            a[i] = a[a[i]];
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+    private static Object[][] exchangeArr(Object[][] arr){
+        if (arr.length == 0) {
+            return new Object[0][0];
+        }
+        Object[][] objects = new Object[arr[0].length][arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                objects[j][i] = arr[i][j];
+            }
+        }
+        return objects;
+    }
+
+    private static int log2(int N){
+        int count = 0;
+        while (N/2 > 0){
+            count ++;
+            N = N/2;
+        }
+        return count;
+    }
+
+    private static String exR1(int n){
+        if (n <= 0)
+            return "";
+        return exR1(n - 3) + n + exR1(n-2) + n;
     }
 
     public static void main(String[] args) {
@@ -96,13 +139,100 @@ public class FirstExercise {
 ////
 ////        System.out.println(Integer.toOctalString(456));
 ////        System.out.println(toBinaryString(456));
-        boolean[][] arr = new boolean[5][6];
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 6; j++) {
-                arr[i][j] = (i + j) % 2 == 0;
-            }
+//        1.1.11
+//        Boolean[][] arr = new Boolean[5][6];
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < 6; j++) {
+//                arr[i][j] = (i + j) % 2 == 0;
+//            }
+//        }
+//        printArr(arr);
+//        1.1.12
+//        changeArr();
+//        1.1.13
+//        exchangeArr(arr);
+//        1.1.14
+//        System.out.println(log2(64));
+
+//        System.out.println(exR1(6));
+//        System.out.println(mystery(2, 3));
+//        System.out.println(mystery(3, 3));
+//        long startTime = System.currentTimeMillis();
+//        System.out.println(startTime);
+//        for (int i = 0; i < 100; i++) {
+//            // fib 是通过递归的方式计算的斐波那契数列， fibonacci 是通过累加循环的方式计算，试验表明，循环的方式效率高很多，计算时间1ms都不到，但是递归深度到40左右就非常慢了
+//            // 因此通过递归方式计算必须做相关的优化
+//            System.out.println(i + " : " + fibonacci(i));
+//            System.out.println(i + " : " + fib(i, 1, 1));
+//        }
+//        long endTime = System.currentTimeMillis();
+//        System.out.println(endTime);
+//        System.out.println(endTime - startTime);
+//        System.out.println(lnFactorial(20));
+//        int[] arr = {1,4,5,6,8,0,2,11,43,23};
+//        Arrays.sort(arr);
+//        System.out.println(binarySearch(0, 10, arr, 43, 0));
+        System.out.println(euclid(1111111, 1234567));
+    }
+
+    private static int mystery(int a, int b) {
+        if (b == 0)
+            return 1;
+        if (b % 2 == 0)
+            return mystery(a * a, b / 2);
+        return mystery(a * a, b / 2) * a;
+    }
+
+    // 尾递归
+    private static long fib(int N, long a, long b) {
+        if (N == 0) {
+            return 0;
         }
-        printArr(arr);
+        if (N == 1) {
+            return a;
+        }
+        return fib(N - 1, b, a + b);
+    }
+
+    private static long factorial(long n){
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    private static double lnFactorial(long n) {
+        return Math.log(factorial(n));
+    }
+
+    private static int binarySearch(int lo, int hi, int[] arr, int n, int height) {
+        height ++;
+        for (int i = 0; i < height; i++) {
+            System.out.print("\t");
+        }
+        System.out.println("lo:"+lo + ", hi:"+ hi);
+        if (lo > hi) {
+            return -1;
+        }
+        int mid = lo + (hi - lo) / 2;
+        if (arr[mid] > n) {
+
+            return binarySearch(lo, mid, arr, n, height);
+        } else if (arr[mid] < n) {
+
+            return binarySearch(mid, hi, arr, n, height);
+        } else {
+            return mid;
+        }
+    }
+
+    private static int euclid(int a, int b){
+        System.out.println("a:" + a + ", b:" + b);
+        if (b == 0) {
+            return a;
+        }
+        int r = a % b;
+        return euclid(b, r);
     }
 
 
